089d4aaa29e21e223ace78f4ca6df6f3e84739a6
errayn-hub <128374386+errayn-hub@users.noreply.github.com>
2023-04-11 15:38:06 +0300

    Initial Commit

---
+++ ImageCard/ImageCardApp.swift
@@ -0,0 +1,17 @@
+//
+//  ImageCardApp.swift
+//  ImageCard
+//
+//  Created by ErRayn on 11.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct ImageCardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
