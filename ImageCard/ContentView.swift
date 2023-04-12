55a4ecb19adcef567f9f04283e5f545010de4e6b
errayn-hub <128374386+errayn-hub@users.noreply.github.com>
2023-04-12 09:29:49 +0300

    logo design

--- ImageCard/ContentView.swift
+++ ImageCard/ContentView.swift
@@ -9,16 +9,9 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
-        }
-        .padding()
+        ImageCard()
     }
 }
-
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
--- ImageCard/ImageCard.swift
+++ ImageCard/ImageCard.swift
@@ -9,8 +9,35 @@ import SwiftUI
 
 struct ImageCard: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        
+                VStack(alignment: .leading, spacing: 16.0){
+                    Image("venüs")
+                        .resizable()
+                        .frame(width: 200, height: 200)
+                    cardText.padding(.horizontal, 8)
+                }
+                .background(Color.white)
+                .clipShape(RoundedRectangle(cornerRadius: 24.0))
+                .shadow(radius: 8)
+            }
+            
+    var cardText: some View{
+        VStack(alignment: .leading) {
+            HStack(spacing: 4.0){
+                Image(systemName: "v.square.fill")
+                Text("Venüs Cafe")
+                    .font(.headline)
+                    .frame(width: 100)
+            }.padding()
+            HStack(spacing: 4.0){
+                Image(systemName: "pencil")
+                Text("FeedBack")
+                
+            }.foregroundColor(.blue)
+                .padding(.bottom,16)
+        }
     }
+    
 }
 
 struct ImageCard_Previews: PreviewProvider {
