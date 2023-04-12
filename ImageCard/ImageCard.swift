bf877ea1f899389115dd13296636376419dae6c6
errayn-hub <128374386+errayn-hub@users.noreply.github.com>
2023-04-12 09:27:00 +0300

    first commit

---
+++ ImageCard/ImageCard.swift
@@ -0,0 +1,20 @@
+//
+//  ImageCard.swift
+//  ImageCard
+//
+//  Created by ErRayn on 11.04.2023.
+//
+
+import SwiftUI
+
+struct ImageCard: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ImageCard_Previews: PreviewProvider {
+    static var previews: some View {
+        ImageCard()
+    }
+}
